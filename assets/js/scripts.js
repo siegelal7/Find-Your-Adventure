e1a750924f9c6b399c316937b2819c983c8d34aa
Andrew Siegel <siegelal7@gmail.com>
2020-09-16 13:59:01 -0400

    added dom elements variables

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,27 +1,28 @@
-
-
 /**
  * GLOBAL VARIABLES
  */
 
-
- /**
+/**
  * DOM ELEMENTS
  */
-
-
-
- /**
+var landingPageHeader = $("#landingPageHeader");
+var assessmentBtn = $("#assessmentBtn");
+var distanceBtn = $("#distanceBtn");
+var activityBtn = $("#activityBtn");
+var assessmentDiv = $("#assessmentDiv");
+var distanceDiv = $("#distanceDiv");
+var activityDiv = $("#activityDiv");
+var parkListDiv = $("#parkListDiv");
+var parkDiv = $("#parkDiv");
+var container = $("#container");
+/**
  * FUNCTION DEFINITIONS
  */
 
-
- /**
+/**
  * FUNCTION   CALLS
  */
 
-
-
- /**
+/**
  * EVENT HANDLERS
  */
