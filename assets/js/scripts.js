6ce4c6e8af4c835bc4813c04ffb1de7a442cfb18
Zach Drummond <zachdrummond3@gmail.com>
2020-09-16 17:15:55 -0400

    Added assessment question and buttons

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   /**
    * GLOBAL VARIABLES
    */
-  console.log("Hello Paradise");
+    var topicsArray = [""];
 
   /**
    * DOM ELEMENTS
@@ -36,7 +36,20 @@ $(document).ready(function () {
     clearScreen();
   });
 
-  assessmentBtn.on("click", function () {});
+  // Event Listener - Loading Page Assessment Button
+  assessmentBtn.on("click", function () {
+    clearScreen();
+
+    var questionHeader = $("<h1>");
+    questionHeader.text("Which topic would you like to explore?");
+
+    var option1 = $("<button type='button' class='btn btn-primary'>Option1</button>");
+    var option2 = $("<button type='button' class='btn btn-primary'>Option2</button>");
+    var option3 = $("<button type='button' class='btn btn-primary'>Option3</button>");
+    var option4 = $("<button type='button' class='btn btn-primary'>Option4</button>");
+    assessmentDiv.append(questionHeader, option1, option2, option3, option4);
+    assessmentDiv.attr("class", ".display");
+  });
 
   // Function to add event listener to distance button
   distanceBtn.on("click", function (event) {
